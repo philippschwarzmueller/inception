0354968fccf8867729ee22e071eca4865d2bd4a2
pschwarz <pschwarz@student.42heilbronn.de>
2023-08-08 16:49:24 +0200

    fastcgi working, missing connection to mariadb

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,6 +8,10 @@ cd "/usr/share/webapps/"
 wget "http://wordpress.org/latest.tar.gz"
 tar -xzvf latest.tar.gz
 rm latest.tar.gz
-ln -s /usr/share/webapps/wordpress/ /var/www/
+#ln -s /usr/share/webapps/wordpress/ /var/www/
 # in /etc/php81/php-fpm.d/www.conf change listen to 9000 only - thanks ole
 # php-fpm81 - to start the fpm
+sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
+sleep 1
+#needs to be run in foreground entrypoint later
+php-fpm81
