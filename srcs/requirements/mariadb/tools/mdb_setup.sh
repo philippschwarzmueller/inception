3ad69ec788d0fd886c128c9a6353c42bb4aa0dec
pschwarz <pschwarz@student.42heilbronn.de>
2023-08-10 18:16:17 +0200

    mariadb running with users, script files of wp not loaded via http

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -1,6 +1,12 @@
 $MARIA_DB_SETUP = `
-
+CREATE DATABSE wp_db;
+CREATE USER 'wp-user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wp_db.* TO 'wp-user'@'%';
+FLUSH PRIVILEGES;
 `
+#ALTER USER 'root'@'localhost' IDENTIFIED BY 'root-pwd';
 
 rc-service mariadb start
 mariadb < MARIA_DB_SETUP
+rc-service mariadb stop
+#mariadbd -u root --bind-address=0.0.0.0
