1cf4c14ae8e4a618093ae5a4b8b2897463c839ee
pschwarz <pschwarz@student.42heilbronn.de>
2023-08-11 16:18:56 +0200

    automatic setup of mariadb

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -1,12 +1,11 @@
-$MARIA_DB_SETUP = `
-CREATE DATABSE wp_db;
+#!/bin/sh
+echo > maria "CREATE DATABASE wp_db;
 CREATE USER 'wp-user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON wp_db.* TO 'wp-user'@'%';
-FLUSH PRIVILEGES;
-`
+FLUSH PRIVILEGES;"
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'root-pwd';
 
-rc-service mariadb start
-mariadb < MARIA_DB_SETUP
-rc-service mariadb stop
-#mariadbd -u root --bind-address=0.0.0.0
+rc-service mariadb start;
+mariadb -u root < maria;
+mysqladmin -u root shutdown;
+mariadbd -u root --bind-address=0.0.0.0;
