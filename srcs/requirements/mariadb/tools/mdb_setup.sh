ad2d9f76518bf1ba523aa84e01a971d44ae34071
Philipp Schwarzmueller <philipp.schwarzmueller@gmail.com>
2023-08-14 15:56:19 +0200

    use env variables for DB and WP

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo > maria "CREATE DATABASE wp_db;
-CREATE USER 'wp-user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wp_db.* TO 'wp-user'@'%';
-FLUSH PRIVILEGES;"
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'root-pwd';
+echo > maria "CREATE DATABASE ${DB_NAME};
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
 
 rc-service mariadb start;
 mariadb -u root < maria;
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -16,6 +16,18 @@ sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
 sleep 10
 #needs to be run in foreground entrypoint later
 #maybe add force on config create
-wp config create --allow-root --dbname=wp_db --dbuser=wp-user --dbpass=password --dbhost=srcs-mariadb-1:3306 --force
-wp core install --title=test --url=localhost --admin_user=wp-admin --admin_password=admin-pass --admin_email=admin@email.de --allow-root
+wp config create --allow-root --force \
+					--dbname=${DB_NAME} \
+					--dbuser=${DB_USER} \
+					--dbpass=${DB_PASS} \
+					--dbhost=srcs-mariadb-1:3306
+wp core install --allow-root \
+				--title=${WP_TITLE} \
+				--url=${WP_URL} \
+				--admin_user=${WP_ADMIN_USER} \
+				--admin_password=${WP_ADMIN_PASS} \
+				--admin_email=${WP_ADMIN_EMAIL}
+wp user create ${WP_USER} ${WP_USER_EMAIL} \
+			--allow-root \
+			--user_pass=${WP_USER_PASS}
 php-fpm81 -F
